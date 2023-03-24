f2349af29d1225998392da6fd912514a8a82c066
Hanakla <8597982+hanakla@users.noreply.github.com>
2023-03-25 00:57:11 +0900

    Fix lint

--- src/components/ModalBase.tsx
+++ src/components/ModalBase.tsx
@@ -11,74 +11,75 @@ type Props = {
   onClose: (value: null) => void;
 };
 
-export const ModalBase = forwardRef<HTMLDivElement, Props>(
-  ({ className, header, content, footer, onClose }, ref) => {
-    const onBackdropClick = useFunc((e: MouseEvent) => {
-      if (e.target === e.currentTarget) onClose(null);
-    });
+export const ModalBase = forwardRef<HTMLDivElement, Props>(function ModalBase(
+  { className, header, content, footer, onClose },
+  ref
+) {
+  const onBackdropClick = useFunc((e: MouseEvent) => {
+    if (e.target === e.currentTarget) onClose(null);
+  });
 
-    return (
+  return (
+    <div
+      ref={ref}
+      css={css`
+        position: fixed;
+        top: 0;
+        left: 0;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        z-index: 1;
+        background-color: ${rgba('#000', 0.5)};
+      `}
+      className={className}
+      onClick={onBackdropClick}
+    >
       <div
-        ref={ref}
-        css={css`
-          position: fixed;
-          top: 0;
-          left: 0;
-          display: flex;
-          width: 100%;
-          height: 100%;
-          justify-content: center;
-          z-index: 1;
-          background-color: ${rgba('#000', 0.5)};
+        css={`
+          flex: none;
+          width: 50vw;
+          min-width: 300px;
+          margin: auto;
+          border-radius: 8px;
+          background-color: #fff;
+          box-shadow: 0 0 16px ${rgba('#222', 0.2)};
         `}
-        className={className}
-        onClick={onBackdropClick}
       >
-        <div
-          css={`
-            flex: none;
-            width: 50vw;
-            min-width: 300px;
-            margin: auto;
-            border-radius: 8px;
-            background-color: #fff;
-            box-shadow: 0 0 16px ${rgba('#222', 0.2)};
-          `}
-        >
-          {header && (
-            <div
-              css={`
-                padding: 16px 16px 0;
-                h1 {
-                  text-align: center;
-                  font-weight: bold;
-                  font-size: 16px;
-                }
-              `}
-            >
-              {header}
-            </div>
-          )}
-
+        {header && (
           <div
             css={`
-              padding: 16px;
+              padding: 16px 16px 0;
+              h1 {
+                text-align: center;
+                font-weight: bold;
+                font-size: 16px;
+              }
             `}
           >
-            {content}
+            {header}
           </div>
+        )}
 
-          {footer && (
-            <div
-              css={`
-                padding: 0 16px 16px;
-              `}
-            >
-              {footer}
-            </div>
-          )}
+        <div
+          css={`
+            padding: 16px;
+          `}
+        >
+          {content}
         </div>
+
+        {footer && (
+          <div
+            css={`
+              padding: 0 16px 16px;
+            `}
+          >
+            {footer}
+          </div>
+        )}
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
