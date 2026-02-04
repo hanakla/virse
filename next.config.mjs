0e8c3a5c05c858e89c238314eeb699f506e841d9
Hanakla <8597982+hanakla@users.noreply.github.com>
2026-02-05 03:33:34 +0900

    知らね〜〜〜〜〜！！！

--- next.config.mjs
+++ next.config.mjs
@@ -18,9 +18,6 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /.wasm$/,
